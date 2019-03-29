ad8a58f221ceb114c9de850f7ee756bdba370172
CHENGCHANGHU <1669675499@qq.com>
2019-03-29 09:34:39 +0800

    cch

--- app/src/main/java/com/example/helloworld/HttpUtil.java
+++ app/src/main/java/com/example/helloworld/HttpUtil.java
@@ -1,11 +1,22 @@
 package com.example.helloworld;
 
+import android.os.Message;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class HttpUtil {
+    public static String BASEURL_LOGIN_SIGN_PRODUCE = "http://223.3.79.211:8000";
+    public static String BASEURL_SELL = "http://223.3.79.119:8000";
+
     public static final MediaType JSON
             = MediaType.get("application/json; charset=utf-8");
 
@@ -51,5 +62,34 @@ public class HttpUtil {
             }
         }).start();
     }
-
 }
+
+//    public static void uploadFile(final String url) {
+//        //1.创建对应的MediaType
+//        private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+//        private final OkHttpClient client = new OkHttpClient();
+//
+//        public void uploadImage (String userName, File file) throws NetworkException {
+//
+//            //2.创建RequestBody
+//            RequestBody fileBody = RequestBody.create(MEDIA_TYPE_PNG, file);
+//
+//            //3.构建MultipartBody
+//            RequestBody requestBody = new MultipartBody.Builder()
+//                    .setType(MultipartBody.FORM)
+//                    .addFormDataPart("file", "testImage.png", fileBody)
+//                    .addFormDataPart("userName", userName)
+//                    .build();
+//
+//            //4.构建请求
+//            Request request = new Request.Builder()
+//                    .url("http://xxxxx")
+//                    .post(requestBody)
+//                    .build();
+//
+//            //5.发送请求
+//            Response response = client.newCall(request).execute();
+//        }
+//
+//
+//    }
