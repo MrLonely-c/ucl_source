71eaebfd78f62667df03bf764405ce555b8446f5
CHENGCHANGHU <1669675499@qq.com>
2019-04-04 19:47:13 +0800

    企业注册编辑框id添加

--- app/src/main/java/com/example/helloworld/baseuser/BaseUserSignUpActivity.java
+++ app/src/main/java/com/example/helloworld/baseuser/BaseUserSignUpActivity.java
@@ -1,9 +1,10 @@
 package com.example.helloworld.baseuser;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.icu.util.LocaleData;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,9 +13,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.example.helloworld.CodeUtil;
-import com.example.helloworld.JsonUtil;
 import com.example.helloworld.HttpUtil;
+import com.example.helloworld.JsonUtil;
 import com.example.helloworld.R;
 import com.example.helloworld.loginAndSign.LoginAndSignActivity;
 
@@ -37,6 +39,8 @@ public class BaseUserSignUpActivity extends AppCompatActivity
     private EditText etxRePassword = null;
     private EditText etxVerificationCode = null;
 
+    private String vCode = "";
+
     private Button btnGetVerificationCode = null;
     private ImageView ivVerification = null;
     private Button btnBaseUserSignUp = null;
@@ -47,13 +51,6 @@ public class BaseUserSignUpActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_base_user_sign_up);
-
-        TextView btnback=findViewById(R.id.toolbar_left_tv);
-        btnback.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                onBackPressed();
-            }
-        });
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
@@ -62,48 +59,42 @@ public class BaseUserSignUpActivity extends AppCompatActivity
     }
 
     private void initUI() {
-        Intent intent = getIntent();
-
-
         etxUserName = findViewById(R.id.etxUserName);
         etxContact = findViewById(R.id.etxContact);
         etxPassword = findViewById(R.id.etxPassword);
         etxRePassword = findViewById(R.id.etxRePassword);
         etxVerificationCode = findViewById(R.id.etxVerificationCode);
 
-//        btnGetVerificationCode = findViewById(R.id.btn_getVerificationCode);
         ivVerification = findViewById(R.id.iv_verification);
         ivVerification.setOnClickListener(this);
         btnBaseUserSignUp = findViewById(R.id.btn_baseUserSignUp);
         btnBaseUserSignUp.setOnClickListener(this);
 
         ivVerification.setImageBitmap(CodeUtil.getInstance().createBitmap());
+        vCode = CodeUtil.getInstance().getCode();
         Toast.makeText(this, CodeUtil.getInstance().getCode(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-//            case R.id.btn_getVerificationCode:
-//                Log.d(TAG, "获取验证码");
-//                break;
 
             case R.id.iv_verification:
                 ivVerification.setImageBitmap(CodeUtil.getInstance().createBitmap());
+                vCode = CodeUtil.getInstance().getCode();
                 Toast.makeText(this, CodeUtil.getInstance().getCode(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_baseUserSignUp:
                 Log.d(TAG, "提交注册");
-                if (baseUserSignUp()) {
-                    intent = new Intent(BaseUserSignUpActivity.this, LoginAndSignActivity.class);
-                    startActivity(intent);
-                }
+                baseUserSignUp();
+                Log.d(TAG, "提交注册2");
                 break;
 
         }
     }
 
     private boolean baseUserSignUp() {
+        Log.d(TAG, "baseUserSignUp: in");
         boolean access = false;
 
         String userName = etxUserName.getText().toString();
@@ -112,19 +103,20 @@ public class BaseUserSignUpActivity extends AppCompatActivity
         String rePassword = etxRePassword.getText().toString();
         String verificationCode = etxVerificationCode.getText().toString();
 
-        Log.d(TAG, "baseUserSignUp: ");
-        //http://223.3.72.161/register??characterFlag=1
-        HttpUtil.sendOKHttp3RequestPOST("http://223.3.79.211:8000/register?CharacterFlag=1",
+        if (!rePassword.equals(password)) {
+            Toast.makeText(this, "密码不一致", Toast.LENGTH_SHORT).show();
+            return access;
+        } else if (!(verificationCode.toLowerCase()).equals(vCode.toLowerCase())) {
+            Toast.makeText(this, "验证码有误:" + verificationCode.toLowerCase() + ":" + vCode.toLowerCase(), Toast.LENGTH_SHORT).show();
+            return access;
+        }
+
+        HttpUtil.sendOKHttp3RequestPOST(
+                HttpUtil.BASEURL_LOGIN_SIGN_PRODUCE + "/user/register?CharacterFlag=1",
                 JsonUtil.getJSON(
-                        "ConsumerName","fan",
-                "ContactNo","13333337899",
-                        "Password","123456"
-
-
-
-
-
-//                        "password", passwordSS
+                        "ConsumerName", userName,
+                        "ContactNo", Integer.parseInt(contact),
+                        "Password", password
                 ),
                 new Callback() {
                     @Override
@@ -135,7 +127,7 @@ public class BaseUserSignUpActivity extends AppCompatActivity
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         final String resStr = response.body().string();
-
+                        Log.d(TAG, "response.code: " + response.code());
                         Log.d(TAG, "resStr: " + resStr);
                         try {
                             JSONObject resJson = new JSONObject(resStr);
@@ -147,19 +139,20 @@ public class BaseUserSignUpActivity extends AppCompatActivity
                             }
                         } catch (JSONException e) {
                             Log.d(TAG, "JSONException: " + e.toString());
-                            runOnUiThread(new Thread(new Runnable() {
+                            etxContact.setText("");
+                            runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(BaseUserSignUpActivity.this, ""+resStr, Toast.LENGTH_SHORT).show();
+
+                                    Toast.makeText(BaseUserSignUpActivity.this, resStr, Toast.LENGTH_SHORT).show();
                                 }
-                            }));
-//                            e.printStackTrace();
+                            });
+
+                            e.printStackTrace();
                         }
                     }
                 }
-
         );
-
         return access;
     }
 }
